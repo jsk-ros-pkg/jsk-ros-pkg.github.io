0f72037bc21e41161c8c0d4972fd8a7a3d08701d
Ryohei Ueda <garaemon@gmail.com>
2014-03-07 14:16:16 +0900

    listup all the repositories

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,5 +1,33 @@
 // index.js
 
 $(function() {
-  console.log('Hello World');
+  // get all the repository
+  var ORG_NAME = 'jsk-ros-pkg';
+  var END_POINT_PREFIX = 'https://api.github.com/';
+
+  function githubAPI(api) {
+    var url = END_POINT_PREFIX + api;
+    var deferred = new $.Deferred;
+    console.log('accessing to ' + url);
+    $.getJSON(url, function(result) {
+      deferred.resolve(result);
+    });
+    return deferred.promise()
+  };
+
+  var repo_info_short_ejs = $('#repo-info-short').html();
+  githubAPI('orgs/' + ORG_NAME + '/repos')
+    .then(function(repos) {
+      repos.sort(function(a, b) {
+        return a.name > b.name;
+      });
+      console.log(repos.length + ' repository at ' + ORG_NAME);
+      _.forEach(repos, function(repo) {
+        var name = repo.name;
+        var ejs_data = {name: name};
+        var html = ejs.render(repo_info_short_ejs, ejs_data);
+        $('#repository-list').append(html);
+      });
+    });
+  
 });
