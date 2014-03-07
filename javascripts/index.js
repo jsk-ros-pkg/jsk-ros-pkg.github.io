d0f62fb118defbbfa8e29cc5f057d2dae812216f
Ryohei Ueda <garaemon@gmail.com>
2014-03-07 14:36:10 +0900

    add info about the number of the open issues, the number of the pull req and the build status of travis

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,9 +2,7 @@
 
 $(function() {
   // get all the repository
-  var ORG_NAME = 'jsk-ros-pkg';
   var END_POINT_PREFIX = 'https://api.github.com/';
-
   function githubAPI(api) {
     var url = END_POINT_PREFIX + api;
     var deferred = new $.Deferred;
@@ -14,20 +12,31 @@ $(function() {
     });
     return deferred.promise()
   };
-
   var repo_info_short_ejs = $('#repo-info-short').html();
-  githubAPI('orgs/' + ORG_NAME + '/repos')
-    .then(function(repos) {
-      repos.sort(function(a, b) {
-        return a.name > b.name;
-      });
-      console.log(repos.length + ' repository at ' + ORG_NAME);
-      _.forEach(repos, function(repo) {
-        var name = repo.name;
-        var ejs_data = {name: name};
-        var html = ejs.render(repo_info_short_ejs, ejs_data);
-        $('#repository-list').append(html);
-      });
-    });
   
+  var ORG_NAME = 'jsk-ros-pkg';
+  
+  function listRepository(org_name, list_id) {
+    githubAPI('orgs/' + org_name + '/repos')
+      .then(function(repos) {
+        repos.sort(function(a, b) {
+          return a.name > b.name;
+        });
+        console.log(repos.length + ' repository at ' + org_name);
+        _.forEach(repos, function(repo) {
+          console.log(repo);
+          var name = repo.name;
+          githubAPI('repos/' + org_name + '/' + repo.name + '/pulls')
+            .then(function(pulls) {
+              var ejs_data = {name: name, issue_num: repo.open_issues_count,
+                              pr_num: pulls.length,
+                              org_name: org_name};
+              var html = ejs.render(repo_info_short_ejs, ejs_data);
+              $(list_id + ' ul').append(html);
+            });
+        });
+      });
+  };
+  listRepository('jsk-ros-pkg', '#jsk-ros-pkg-info');
+  listRepository('start-jsk', '#start-jsk-info');
 });
